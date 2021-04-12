4655a255d0b9b6b59f05a959eed1ddfc1a22009d
Marian Jarnik <jarnik.m@gmail.com>
2021-04-12 17:03:42 +0200

    initialize

---
+++ src/sk/stu/fiit/model/Category.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.stu.fiit.model;
+
+/**
+ *
+ * @author jarni
+ */
+public class Category {
+    private String name;
+    private String desc;
+    private double price;
+
+    public Category(String name, String desc, double price) {
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    
+    
+    
+}
